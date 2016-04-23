39dcef8683e2b12eed1649c1084c9d2b1c2348bb
Collin Alligood <collin.alligood@gmail.com>
2016-04-22 19:45:59 -0700

    Drawline to player

--- src/main.js
+++ src/main.js
@@ -84,6 +84,10 @@ function Survivor(game, spritesheet) {
     this.myDir = 0;
     this.ctx = game.ctx;
     var that = this;
+
+    var mouseX = 0;
+    var mouseY = 0;
+
     this.ctx.canvas.addEventListener("keydown", function (e) {
         if (e.code === "KeyD") {
             that.d = true;
@@ -121,9 +125,12 @@ function Survivor(game, spritesheet) {
         }
         }, false);
     this.ctx.canvas.addEventListener("mousemove", function (e) {
-        var x = e.x - that.x - 129 * that.animation.scale;
-        var y = e.y - that.y - 110 * that.animation.scale;
-        that.myAngle = 360 - Math.atan2(x, y) % 360;
+        that.mouseX = e.x - 8;
+        that.mouseY = e.y - 8;
+        var x = (e.x - that.x + (that.animation.frameWidth/2)) * that.animation.scale;
+        var y = (e.y - that.y + (that.animation.frameHeight/2)) * that.animation.scale;
+        that.myAngle = (Math.atan2(y, x) * 180 / Math.PI) % 360;
+        console.log(that.myAngle + "\n\n");
     },false);
     Entity.call(this, game, 0, 250);
 }
@@ -167,6 +174,10 @@ Survivor.prototype.update = function () {
 
 Survivor.prototype.draw = function () {
     this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
+    this.game.ctx.beginPath();
+    this.game.ctx.moveTo(this.x + ((this.animation.frameWidth/2) * this.animation.scale), this.y + ((this.animation.frameHeight/2) * this.animation.scale));
+    this.game.ctx.lineTo(this.mouseX, this.mouseY);
+    this.game.ctx.stroke();
     Entity.prototype.draw.call(this);
 }
 
