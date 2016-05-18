17933d9ea756ce437f63cf59a186204aa5753b97
mbadgett <m_badgett@hotmail.com>
2016-05-18 01:02:01 -0700

    Updated maze collision detection. very close to perfect collision handling

--- src/main.js
+++ src/main.js
@@ -232,6 +232,44 @@ Survivor.prototype.checkWalls = function () {
     var i = Math.floor((this.x + 50) / 400);
     var j = Math.floor((this.y + 50) / 400);
     var currentCell = this.game.maze.grid[i][j];
+
+    /*
+    First we are checking the gaps between our main cells that we draw.
+    we must update the mouse xy relative to the player.
+    */
+    if ((this.x + this.animation.frameWidth * this.animation.scale / 2 > 400 * i + 300) &&
+        (this.y + this.animation.frameHeight * this.animation.scale / 4 > 400 * j + 262.5)) {
+        var difY = this.y + this.animation.frameHeight * this.animation.scale / 4 - (400 * j + 262.5);
+        this.y -= difY;
+        this.mouseY -= difY;
+    }
+
+    if ((this.x + this.animation.frameWidth * this.animation.scale / 2 > 400 * i + 300) &&
+        (this.y + this.animation.frameHeight * this.animation.scale / 4 < 400 * j)) {
+        var difY = (400 * j) - (this.y + this.animation.frameHeight * this.animation.scale / 4);
+        this.y += difY;
+        this.mouseY += difY;
+    }
+
+    if ((this.y + this.animation.frameHeight * this.animation.scale / 2 > 400 * j + 300) &&
+        (this.x + this.animation.frameWidth * this.animation.scale / 4 > 400 * i + 250)) {
+        var difX = this.x + this.animation.frameWidth * this.animation.scale / 4 - (400 * i + 250);
+        this.x -= difX;
+        this.mouseX -= difX;
+    }
+
+    if ((this.y + this.animation.frameHeight * this.animation.scale / 2 > 400 * j + 300) &&
+        (this.x + this.animation.frameWidth * this.animation.scale / 4 < 400 * i)) {
+        var difX = (400 * i) - (this.x + this.animation.frameWidth * this.animation.scale / 4);
+        this.x += difX;
+        this.mouseX += difX;
+    }
+
+
+    /*
+    Now we check collision in the main cell walls.
+    we must also update the mouse xy.
+     */
     if (!currentCell.east && this.x + this.animation.frameWidth / 2 > 400 * i + 350) {
         var difX = this.x + this.animation.frameWidth / 2 - (400 * i + 350);
         this.x -= difX;
@@ -341,7 +379,7 @@ AM.downloadAll(function () {
     var ctx = canvas.getContext("2d");
 
     var gameEngine = new GameEngine();
-    gameEngine.initMaze(20);
+    gameEngine.initMaze(10);
     gameEngine.init(ctx);
     gameEngine.start();
 
